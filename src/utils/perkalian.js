acb19122e1d4f20033acd8b8bed133219511a9f2
ivan mufid <ivanmohammad2355@gmail.com>
2022-03-31 18:10:06 +0700

    Section 16

---
+++ src/utils/perkalian.js
@@ -0,0 +1,3 @@
+export const pembagian = (variabelSatu, variabelDua) => {
+  return variabelSatu * variabelDua
+}
--- tests/unit/operations.spec.js
+++ tests/unit/operations.spec.js
@@ -1,8 +1,9 @@
 import { penjumlahan } from '@/utils/penjumlahan.js'
 import { pengurangan } from '@/utils/pengurangan.js'
 import { pembagian } from '@/utils/pembagian.js'
+import { perkalian } from '@/utils/perkalian.js'
 
-describe('Melakukan Penjumlahan', () => {
+describe('Melakukan Perhitungan Operasi Bilangan', () => {
   test('Penjumlahan dua bilangan positif', () => {
     expect(penjumlahan(2, 4)).toBe(6)
   })
@@ -11,5 +12,8 @@ describe('Melakukan Penjumlahan', () => {
   })
   test('Pembagian dua bilangan positif', () => {
     expect(pembagian(16, 4)).toBe(4)
+  })
+  test('Perkalian dua bilangan positif', () => {
+    expect(perkalian(2, 4)).toBe(8)
   })
 })
