4dd8291d7f6e6f872ab5a0152e75628e1ae30b18
ivan mufid <ivanmohammad2355@gmail.com>
2022-03-31 18:04:52 +0700

    Section 16

--- tests/unit/operations.spec.js
+++ tests/unit/operations.spec.js
@@ -1,7 +1,15 @@
 import { penjumlahan } from '@/utils/penjumlahan.js'
+import { pengurangan } from '@/utils/pengurangan.js'
+import { pembagian } from '@/utils/pembagian.js'
 
 describe('Melakukan Penjumlahan', () => {
   test('Penjumlahan dua bilangan positif', () => {
     expect(penjumlahan(2, 4)).toBe(6)
+  })
+  test('Pengurangan dua bilangan positif', () => {
+    expect(pengurangan(9, 4)).toBe(5)
+  })
+  test('Pembagian dua bilangan positif', () => {
+    expect(pembagian(16, 4)).toBe(4)
   })
 })
